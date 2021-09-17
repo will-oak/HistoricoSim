594f96ce7f304068accd0874c252e23c06290bd9
William <william.pereira6@fatec.sp.gov.br>
2021-09-16 21:46:18 -0300

    first commit

---
+++ src/controller/HistoricoController.java
@@ -0,0 +1,31 @@
+package controller;
+
+import com.willoak.utils.pilha.string.Pilha;
+
+public class HistoricoController {
+
+	public HistoricoController() {
+
+		}
+    public void insertUrl(Pilha pilha, String url) { 
+
+
+    }
+    public void consultUrl(Pilha pilha) { 
+
+
+    }
+    public void removeUrl(Pilha pilha) { 
+
+
+    }
+    
+    public static boolean validateUrl(String url) {
+    	
+    }
+    
+}
+
+
+
+
---
+++ src/view/Principal.java
@@ -0,0 +1,17 @@
+package view;
+
+import javax.swing.JOptionPane;
+
+import com.willoak.utils.pilha.string.Pilha;
+
+
+
+public class Principal {
+     
+	public static void main(String[] args) {
+   
+	String url = JOptionPane.showInputDialog(null, "Insira a URL:");
+	
+
+}
+}
