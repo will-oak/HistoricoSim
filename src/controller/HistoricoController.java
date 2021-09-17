0533bdff43f3dd38d3f03bc0f7d3fa625d27586e
William <william.pereira6@fatec.sp.gov.br>
2021-09-16 22:10:30 -0300

    commit

--- src/controller/HistoricoController.java
+++ src/controller/HistoricoController.java
@@ -1,31 +1,64 @@
 package controller;
 
+import javax.swing.JOptionPane;
+
 import com.willoak.utils.pilha.string.Pilha;
 
 public class HistoricoController {
 
 	public HistoricoController() {
-
+		
+	}
+	
+	public void insertHistory(Pilha pilha, String url) {
+		if (validateUrl(url)) {
+			pilha.push(url);
+			JOptionPane.showMessageDialog(null, "Site " + url + " inserido");
+		} else {
+			JOptionPane.showMessageDialog(null, "Site " + url + " inválido. A url deve seguir o exemplo: https://www.example.com");
 		}
-    public void insertUrl(Pilha pilha, String url) { 
+		
+	}
+	
+	public void consultHistory(Pilha pilha) {
+		try {
+			JOptionPane.showMessageDialog(null, pilha.top());
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Histórico vazio");
+		}
+		
+	}
+	
+	public void removeHistory(Pilha pilha) {
+		try {
+			String item = pilha.pop();
+			JOptionPane.showMessageDialog(null, "Site " + item + " removido");
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Histórico vazio");
+		}
+	}
+	
+	public static boolean validateUrl(String url) {
+		String[] dominios = new String[]{ ".com", ".br", ".org" };
+		
+		if (url.contains("http://") && url.contains("www.")) {
+			int dominioCont = 0;
+			
+			for (int i = 0; i < dominios.length; i ++) {
+				if (url.contains(dominios[i])) {
+					dominioCont++;
+				}
+			}
+			
+			if (dominioCont == 0) {
+				return false;
+			}
+			
+		} else {
+			return false;
+		}
+		
+		return true;
+	}
 
-
-    }
-    public void consultUrl(Pilha pilha) { 
-
-
-    }
-    public void removeUrl(Pilha pilha) { 
-
-
-    }
-    
-    public static boolean validateUrl(String url) {
-    	
-    }
-    
 }
-
-
-
-
--- src/view/Principal.java
+++ src/view/Principal.java
@@ -5,13 +5,38 @@ import javax.swing.JOptionPane;
 import com.willoak.utils.pilha.string.Pilha;
 
 
+import controller.HistoricoController;
 
 public class Principal {
-     
-	public static void main(String[] args) {
-   
-	String url = JOptionPane.showInputDialog(null, "Insira a URL:");
-	
 
-}
+	public static void main(String[] args) {
+		Pilha pilha = new Pilha();
+		
+		HistoricoController historico = new HistoricoController();
+		
+		String[] actionButtons = { "Adicionar histórico", "Consultar último item", "Deletar último item", "Cancelar" };
+		
+		int opcao = 0;
+
+		do {
+			opcao = JOptionPane.showOptionDialog(null, "Qual será a ação?", "Confirmation",
+					JOptionPane.INFORMATION_MESSAGE, 0, null, actionButtons, actionButtons[0]);
+			
+			switch (opcao) {
+				case 0:
+					String url = JOptionPane.showInputDialog(null, "Insira uma Url válida");
+					historico.insertHistory(pilha, url);
+					break;
+				case 1:
+					historico.consultHistory(pilha);
+					break;
+				case 2:
+					historico.removeHistory(pilha);
+					break;
+			}
+
+		} while (opcao != 3);
+
+	}
+
 }
